921be584d2628c2d133e960a23e42755e7d695b3
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-09-10 06:09:46 +0300

    better log configuration

--- src/log.rs
+++ src/log.rs
@@ -1,4 +1,10 @@
-static DEBUG_LOGS_ALLOWED: bool = true;
+const DEBUG_LOGS_ALLOWED_ON_RELEASE: bool = false;
+const DEBUG_LOGS_ALLOWED_ON_DEV: bool = true;
+const DEBUG_LOGS_ALLOWED: bool = if cfg!(debug_assertions) {
+    DEBUG_LOGS_ALLOWED_ON_DEV
+} else {
+    DEBUG_LOGS_ALLOWED_ON_RELEASE
+};
 
 pub fn log_file_name(name: &String) {
     if DEBUG_LOGS_ALLOWED {

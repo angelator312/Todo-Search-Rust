e317beb5df75a900f0570ef825702ea65f1b0213
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-09-10 03:00:03 +0300

    now the extension can compile

--- src/extension.rs
+++ src/extension.rs
@@ -1,13 +1,60 @@
+mod display;
+mod log;
+mod mylib;
+mod structs;
+mod utils;
+
 use zed_extension_api::{
-    self as zed, SlashCommand, SlashCommandArgumentCompletion, SlashCommandOutput,
+    self as zed, Result, SlashCommand, SlashCommandArgumentCompletion, SlashCommandOutput,
     SlashCommandOutputSection, Worktree,
 };
 
-struct MyExtension {}
-impl zed::Extension for MyExtension {
+use crate::structs::TODODir;
+
+struct TestExtension {
+    todo_tree: structs::TODODir,
+}
+
+impl TestExtension {}
+
+impl zed::Extension for TestExtension {
     fn new() -> Self {
-        return MyExtension {};
+        Self {
+            todo_tree: TODODir {
+                name: "Project".to_string(),
+                files: vec![],
+                dirs: vec![],
+            },
+        }
     }
+    fn complete_slash_command_argument(
+        &self,
+        command: SlashCommand,
+        _args: Vec<String>,
+    ) -> Result<Vec<zed_extension_api::SlashCommandArgumentCompletion>, String> {
+        match command.name.as_str() {
+            "echo" => Ok(vec![]),
+            "pick-one" => Ok(vec![
+                SlashCommandArgumentCompletion {
+                    label: "Option One".to_string(),
+                    new_text: "option-1".to_string(),
+                    run_command: true,
+                },
+                SlashCommandArgumentCompletion {
+                    label: "Option Two".to_string(),
+                    new_text: "option-2".to_string(),
+                    run_command: true,
+                },
+                SlashCommandArgumentCompletion {
+                    label: "Option Three".to_string(),
+                    new_text: "option-3".to_string(),
+                    run_command: true,
+                },
+            ]),
+            command => Err(format!("unknown slash command: \"{command}\"")),
+        }
+    }
+
     fn run_slash_command(
         &self,
         command: SlashCommand,
@@ -55,32 +102,6 @@ impl zed::Extension for MyExtension {
             command => Err(format!("unknown slash command: \"{command}\"")),
         }
     }
-
-    fn complete_slash_command_argument(
-        &self,
-        command: SlashCommand,
-        _args: Vec<String>,
-    ) -> Result<Vec<SlashCommandArgumentCompletion>, String> {
-        match command.name.as_str() {
-            "echo" => Ok(vec![]),
-            "pick-one" => Ok(vec![
-                SlashCommandArgumentCompletion {
-                    label: "Option One".to_string(),
-                    new_text: "option-1".to_string(),
-                    run_command: true,
-                },
-                SlashCommandArgumentCompletion {
-                    label: "Option Two".to_string(),
-                    new_text: "option-2".to_string(),
-                    run_command: true,
-                },
-                SlashCommandArgumentCompletion {
-                    label: "Option Three".to_string(),
-                    new_text: "option-3".to_string(),
-                    run_command: true,
-                },
-            ]),
-            command => Err(format!("unknown slash command: \"{command}\"")),
-        }
-    }
 }
+
+zed::register_extension!(TestExtension);
